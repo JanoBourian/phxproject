2fd1de1a7985d8bf5be5b003b6e798ad86cf10d2
JanoBourian <supermath33@icloud.com>
2024-07-10 22:46:54 -0600

    add: pivot tables for books_authors and books_categories

---
+++ bookstore/priv/repo/migrations/20240711042145_pivot_tables.exs
@@ -0,0 +1,24 @@
+defmodule Bookstore.Repo.Migrations.PivotTables do
+  use Ecto.Migration
+
+  def change do
+    create table(:books_authors, primary_key: false) do
+      add :books_id, references(:books, on_delete: :delete_all, type: :binary_id), source: :id
+      add :authors_id, references(:authors, on_delete: :delete_all, type: :binary_id), source: :id
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:books_authors, :books_id)
+    create index(:books_authors, :authors_id)
+
+    create table(:books_categories, primary_key: false) do
+      add :books_id, references(:books, on_delete: :delete_all, type: :binary_id), source: :id
+      add :categories_id, references(:categories, on_delete: :delete_all, type: :binary_id), source: :id
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:books_categories, [:books_id])
+    create index(:books_categories, [:categories_id])
+
+  end
+end
